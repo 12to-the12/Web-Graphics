1a5cb862206ae54aef08e5a8a6eb9a69e7a82fa8
Logan Hillyer <loganthomashillyer@gmail.com>
2022-04-07 13:48:06 -0700

    two

--- main.py
+++ main.py
@@ -2,79 +2,75 @@ from browser import document, window
 
 from math import atan, degrees
 from random import random
-moving = document["button"]
+
 page = document["page"]
-x = 50
-y = 50
-dx = -5
-dy = 0
-gravity = 0.1
-damping = 1.05
-y_damping = damping
-x_damping = damping
- 
-run = None
-ang = 0
-arena_width = page.width - 100
-arena_height = page.height - 100
-print(page.width)
-print(page.height)
-x = random()*arena_width
-x = random()*arena_height
 
-def damp():
-    global dx, dy
-    global x_damping, y_damping
-    dx *= x_damping
-    dy *= y_damping
-def render():
-    global x, dx, y, dy, gravity
-    global arena_width, arena_height
-    global y_damping, x_damping
-    moving.style.transform = f"translate({x}px,{y}px) rotate({angle()}deg)"
-    x += dx
-    y += dy
-    if x>=arena_width:
-        damp()
-        dx *= -1 # right wall
-    if x<=0:
-        damp()
-        dx *= -1 # left wall
-    
-    if y >= arena_height:# floor
-        y = arena_height
-        damp()
-        dy *= -1
-    if y <= 0:# ceiling
-        damp()
-        y = 0
-        dy *= -1
-    
-    dy+=gravity
+class Dynamic():
+    def __init__(self, element, page=page):
+        self.element = element
+        self.x = 50
+        self.y = 50
+        self.dx = -5
+        self.dy = 0
+        self.gravity = 0.1
+        self.damping = 0.95
+        self.y_damping = self.damping
+        self.x_damping = self.damping
+        self.ang = 0
+        self.arena_width = page.width - 100
+        self.arena_height = page.height - 200
+        print(page.width)
+        print(page.height)
+        self.x = random()*self.arena_width
+        self.x = random()*self.arena_height
 
-def angle():
-    global dx, dy, ang
-    # angles are clockwise so god help me
-    if dx>0:
-        if dy>0:# quadrant 4
-            ang = 90 + degrees( atan(dy/dx) )
-        elif dy<0:# quadrant 1
-            ang = degrees( atan(dx/-dy) )
-        else: return ang
-    elif dx<0:
-        if dy>0: # quadrant 3
-            ang = 180 + degrees( atan(-dx/dy) )
-        elif dy<0: # quadrant 2
-            ang = 270 + degrees( atan(-dy/-dx) )
-        else: return ang
-    else: return ang
-    return ang
-    
-
+    def damp(self):
+        self.dx *= self.x_damping
+        self.dy *= self.y_damping
+    def render(self):
+        self.element.style.transform = f"translate({self.x}px,{self.y}px) rotate({self.angle()}deg)"
+        self.x += self.dx
+        self.y += self.dy
+        if self.x>=self.arena_width: # right wall
+            self.damp()
+            self.x = self.arena_width
+            self.dx *= -1 
+        if self.x<=0: # left wall
+            self.damp()
+            self.x = 0
+            self.dx *= -1
+        if self.y >= self.arena_height:# floor
+            self.y = self.arena_height
+            self.damp()
+            self.dy *= -1
+        if self.y <= 0:# ceiling
+            self.damp()
+            self.y = 0
+            self.dy *= -1
+        self.dy+=self.gravity
+    def angle(self):
+        # angles are clockwise so god help me
+        if self.dx>0:
+            if self.dy>0:# quadrant 4
+                self.ang = 90 + degrees( atan(self.dy/self.dx) )
+            elif self.dy<0:# quadrant 1
+                self.ang = degrees( atan(self.dx/-self.dy) )
+            else: return self.ang
+        elif self.dx<0:
+            if self.dy>0: # quadrant 3
+                self.ang = 180 + degrees( atan(-self.dx/self.dy) )
+            elif self.dy<0: # quadrant 2
+                self.ang = 270 + degrees( atan(-self.dy/-self.dx) )
+            else: return self.ang
+        else: return self.ang
+        return self.ang
+movinga = Dynamic(document["entitya"])
+movingb = Dynamic(document["entityb"])
 
 def animloop(t):
-    global run
+    global run, movinga
     run = window.requestAnimationFrame(animloop)
-    render()
+    movinga.render()
+    movingb.render()
 
 run = window.requestAnimationFrame(animloop)
